9074b9418ef520264492a44e41556ba7d2cf4719
Jadon Clegg <jadon.clegg@gmail.com>
2020-12-07 10:52:35 -0700

    Fixed bug for building windows.

--- power/shutdown_windows.go
+++ power/shutdown_windows.go
@@ -6,7 +6,7 @@ import (
 
 // Shutdown shuts down the computer, maybe... haven't tested the Windows version. In theory, it should work.
 // HOWEVER: It will NOT force applications to close, and will possibly run updates. I hate Windows.
-func Shutdown() error {
+func Shutdown(sudo bool) error {
 	cmd := exec.Command("shutdown", "/s", "/t", "0")
 	return cmd.Run()
 }
